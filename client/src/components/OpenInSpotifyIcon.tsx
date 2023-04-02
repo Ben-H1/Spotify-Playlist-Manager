8dfbc516b47924ed61f7b6ab4ae7a7943c27509c
Ben-H1 <benedicthawthorn@gmail.com>
2023-04-02 16:42:00 +0100

    Open in Spotify icon

--- client/src/components/OpenInSpotifyIcon.tsx
+++ client/src/components/OpenInSpotifyIcon.tsx
@@ -1,16 +1,19 @@
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import clsx from 'clsx';
 
 type OpenInSpotifyIconProps = {
     className?: string
+    onClick?: () => void;
 };
 
-const OpenInSpotifyIcon = ({ className }: OpenInSpotifyIconProps) => {
+const OpenInSpotifyIcon = ({ className, onClick }: OpenInSpotifyIconProps) => {
     return (
         <FontAwesomeIcon
-            className={className}
+            className={clsx(onClick && 'cursor-pointer', className)}
             icon={faArrowUpRightFromSquare}
             title='Open in Spotify'
+            onClick={onClick}
         />
     );
 };
